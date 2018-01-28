c8ff4b4c961fa1816f0ccf4ebb65ce91ed05b22a
Leon-Home <772153508@qq.com>
2018-01-28 13:52:55 +0800

    添加微信服务器验证

---
+++ SenparcCourse.Service/CustomMessageHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Senparc.Weixin.MP;
+using Senparc.Weixin.MP.Entities;
+using Senparc.Weixin.MP.MessageHandlers;
+using Senparc.Weixin.MP.Helpers;
+using System.IO;
+using System.Xml.Linq;
+using Senparc.Weixin.MP.Entities.Request;
+using Senparc.Weixin.MP.AppStore;
+
+namespace SenparcCourse.Service
+{
+    public class CustomMessageHandler : MessageHandler<CustomMessageContext>
+    {
+        public CustomMessageHandler(Stream inputStream, PostModel postModel = null, int maxRecordCount = 0, DeveloperInfo developerInfo = null) : base(inputStream, postModel, maxRecordCount, developerInfo)
+        {
+            base.CurrentMessageContext.ExpireMinutes = 10;
+        }
+
+        public CustomMessageHandler(XDocument requestDocument, PostModel postModel = null, int maxRecordCount = 0, DeveloperInfo developerInfo = null) : base(requestDocument, postModel, maxRecordCount, developerInfo)
+        {
+        }
+
+        public CustomMessageHandler(RequestMessageBase requestMessageBase, PostModel postModel = null, int maxRecordCount = 0, DeveloperInfo developerInfo = null) : base(requestMessageBase, postModel, maxRecordCount, developerInfo)
+        {
+        }
+
+        public override IResponseMessageBase DefaultResponseMessage(IRequestMessageBase requestMessage)
+        { 
+            var responseMessage = requestMessage.CreateResponseMessage<ResponseMessageText>();
+            responseMessage.Content = "当前服务器时间：" + DateTime.Now;
+            return responseMessage;
+        }
+    }
+}
---
+++ SenparcCourse/Controllers/WeixinController.cs
@@ -0,0 +1,45 @@
+﻿using Senparc.Weixin.MP;
+using Senparc.Weixin.MP.Entities.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace SenparcCourse.Controllers
+{
+    public class WeixinController:Controller
+    {
+        public static readonly string Token = WebConfigurationManager.AppSettings["WeixinToken"];//与微信公众账号后台的Token设置保持一致，区分大小写。
+        public static readonly string EncodingAESKey = WebConfigurationManager.AppSettings["WeixinEncodingAESKey"];//与微信公众账号后台的EncodingAESKey设置保持一致，区分大小写。
+        public static readonly string AppId = WebConfigurationManager.AppSettings["WeixinAppId"];//与微信公众账号后台的AppId设置保持一致，区分大小写。
+
+        readonly Func<string> _getRandomFileName = () => DateTime.Now.ToString("yyyyMMdd-HHmmss") + Guid.NewGuid().ToString("n").Substring(0, 6);
+
+
+        public WeixinController()
+        {
+
+        }
+
+        /// <summary>
+        /// 微信后台验证地址（使用Get），微信后台的“接口配置信息”的Url填写如：http://sdk.weixin.senparc.com/weixin
+        /// </summary>
+        [HttpGet]
+        [ActionName("Index")]
+        public ActionResult Get(PostModel postModel, string echostr)
+        {
+            if (CheckSignature.Check(postModel.Signature, postModel.Timestamp, postModel.Nonce, Token))
+            {
+                return Content(echostr); //返回随机字符串则表示验证通过
+            }
+            else
+            {
+                return Content("failed:" + postModel.Signature + "," + Senparc.Weixin.MP.CheckSignature.GetSignature(postModel.Timestamp, postModel.Nonce, Token) + "。" +
+                    "如果你在浏览器中看到这句话，说明此地址可以被作为微信公众账号后台的Url，请注意保持Token一致。");
+            }
+        }
+        
+    }
+}
