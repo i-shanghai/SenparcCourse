ea6337c27a646cf6f5628941968a5dd077bfc1c7
i-shanghai <liyang6956@outlook.com>
2018-03-01 17:34:42 +0800

    图片以流的模式和以HttpPostedFileBase的模式上传

--- SenparcCourse/Controllers/RequestController.cs
+++ SenparcCourse/Controllers/RequestController.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Text;
-using System.Web.Helpers;
-using System.Web.Mvc;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Senparc.Weixin.Helpers.Extensions;
 using SenparcCourse.Service.Dtos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
 
 namespace SenparcCourse.Controllers
 {
@@ -68,15 +69,115 @@ namespace SenparcCourse.Controllers
             return Content(html);
         }
 
-
+        /// <summary>
+        /// 直接从URL下载图片
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
         public ActionResult GetImage(string url = "http://sdk.weixin.senparc.com/images/book-cover-front-small-3d-transparent.png")
+        {
+            //string fileName = Server.MapPath("~/App_Data/DownloadImage_{0}.jpg".FormatWith(DateTime.Now.Ticks));
+
+            string filePath = Server.MapPath("~/App_Data/");  //下载保存路径
+            string fileName = Senparc.Weixin.HttpUtility.Get.Download(url, filePath); //执行下载
+            string newFileName = fileName + ".png"; //重命名文件
+
+            System.IO.File.Move(fileName, newFileName);
+
+
+            return Content("图片保存成功");
+        }
+
+
+        #region 使用流的方式从URL下载图片,然后通过流的方式上传。分为 两步
+        /// <summary>
+        /// 使用流的方式从URL下载图片,然后通过流的方式上传。分为 两步
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns> 
+        public ActionResult GetUploadImage(string url = "http://sdk.weixin.senparc.com/images/book-cover-front-small-3d-transparent.png")
         {
             string fileName = Server.MapPath("~/App_Data/DownloadImage_{0}.jpg".FormatWith(DateTime.Now.Ticks));
 
-            Senparc.Weixin.HttpUtility.Get.Download(url, fileName);
+            using (var ms = new MemoryStream())
+            {
+                Senparc.Weixin.HttpUtility.Get.Download(url, ms);
+                ms.Seek(0, SeekOrigin.Begin);  //指针归到0 的位置
 
-            return Content("图片保存：" + fileName);
+                var uploadUrl = "http://localhost:8376/Request/UploadImage";
+                Senparc.Weixin.HttpUtility.RequestUtility.HttpPost(uploadUrl, null, ms, timeOut: 30000);
+            }
+            return Content("图片下载,然后上传完成");
         }
+
+        /// <summary>
+        /// 使用流的方式上传图片
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult UploadImage()
+        {
+            var stream = Request.InputStream; //保存：Post 过来 的二进制流
+
+            //创建新的文件
+            var fileName = Server.MapPath("~/App_Data/UploadImage.jpg");
+            using (var fileStream = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                stream.CopyTo(fileStream);
+                stream.Flush();
+            }
+
+            return Content("图片下载、并以流的方式上传到Server:" + fileName);
+        }
+
+        #endregion
+
+        #region HttpPostedFileBase 的方式上传
+        /// <summary>
+        /// 图片塞进Form中上传
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetAImageAndUpload(string url = "http://sdk.weixin.senparc.com/images/book-cover-front-small-3d-transparent.png")
+        {
+            string filePath = Server.MapPath("~/App_Data/");  //下载保存路径
+            string fileName = Senparc.Weixin.HttpUtility.Get.Download(url, filePath); //先执行下载
+            string newFileName = fileName + ".png"; 
+
+            System.IO.File.Move(fileName, newFileName); //重命名文件
+
+            //修改为Form表单的形式，Post上传
+            var dictionary = new Dictionary<string, string>
+            {
+                { "file", newFileName }
+            };
+
+            var uploadUrl = "http://localhost:8376/Request/UploadFormImage";
+            var uploadResult = Senparc.Weixin.HttpUtility.RequestUtility.HttpPost(uploadUrl, null, null, dictionary);  //再执行上传
+
+            return Content("图片上传完成:<br/>" + uploadResult);
+        }
+
+        /// <summary>
+        /// 以HttpPostedFileBase的方式，接收文件并保存
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult UploadFormImage(HttpPostedFileBase file)
+        {
+            var stream = file.InputStream;
+
+            //创建新的文件
+            var fileName = Server.MapPath("~/App_Data/UploadImageByHttpPostFile.jpg");
+            using (var fileStream = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                stream.CopyTo(fileStream);
+                stream.Flush();
+            }
+
+            return Content("图片下载、并以HttpPostedFile的方式上传到Server:" + fileName);
+        } 
+        #endregion
 
     }
 }
